f484cecee9e93d861683d3d8581813a9fbdef7d5
kbrian1237 <kbrian1237@gmail.com.com>
2025-09-09 15:41:18 +0300

    Add GitHub Pages deployment

--- web/next.config.js
+++ web/next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  basePath: process.env.NODE_ENV === 'production' ? '/Neurolancer' : '',
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'http',
