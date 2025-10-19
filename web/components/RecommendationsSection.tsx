49a052795e4aa1e2dd198bcd2cd567d1017b4e8c
neurolancer-tech <neurolancermail@gmail.com>
2025-10-19 12:26:45 +0300

    Fix recommendations section: show 'Price TBD' instead of $0 and hide 0 likes

--- web/components/RecommendationsSection.tsx
+++ web/components/RecommendationsSection.tsx
@@ -213,13 +213,13 @@ export default function RecommendationsSection({ userProfile }: RecommendationsS
                       ? (item.budget_min && item.budget_max 
                           ? `$${item.budget_min}-$${item.budget_max}`
                           : 'Budget TBD')
-                      : `$${item.price || 0}`
+                      : (item.price && item.price > 0 ? `$${item.price}` : 'Price TBD')
                     }
                   </span>
                 </div>
 
                 {/* Likes for gigs */}
-                {!isFreelancer && item.likes_count !== undefined && (
+                {!isFreelancer && (item.likes_count !== undefined && item.likes_count > 0) && (
                   <div className="flex items-center text-gray-500 dark:text-gray-400">
                     <svg className="w-4 h-4 mr-1" fill="currentColor" viewBox="0 0 20 20">
                       <path d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" />
